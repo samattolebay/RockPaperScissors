1068a73810d006bb556604aa857940653d0137bc
samattolebay <samattolebay@gmail.com>
2023-05-31 00:39:55 +0600

    dorabotki

--- Game.gd
+++ Game.gd
@@ -1,23 +1,37 @@
 extends Node
 
-var item_count = 15
-var min_x = 0
-var max_x = 600
-var min_y = 0
-var max_y = 600
+var max_x = 300
+var max_y = 300
+
+var paper = "Paper"
+var scissors = "Scissors"
+var rock = "Rock"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AudioStreamPlayer.play()
+	var screen_size = get_viewport().get_visible_rect().size
+	var item_count = rand_range(15, 20)
 	for i in range(item_count):
 		var item_instance = load("res://Rock.tscn").instance()
 		
-		var random_x = rand_range(min_x, max_x)
-		var random_y = rand_range(min_y, max_y)
+		var random_x = rand_range(0, screen_size.x)
+		var random_y = rand_range(0, screen_size.y)
 		item_instance.position = Vector2(random_x, random_y)
 		
-		var random_velocity_x = rand_range(min_x, max_x)
-		var random_velocity_y = rand_range(min_y, max_y)
+		var random_velocity_x = rand_range(0, max_x)
+		var random_velocity_y = rand_range(0, max_y)
 		item_instance.linear_velocity = Vector2(random_velocity_x, random_velocity_y)
+		
+		# var type = int(rand_range(0, 3))
+		var type = i
+		
+		if (type % 3 == 0):
+			item_instance.name = paper
+		elif (type % 3 == 1):
+			item_instance.name = rock
+		else:
+			item_instance.name = scissors
 		
 		add_child(item_instance)
 
--- Rock.gd
+++ Rock.gd
@@ -1,10 +1,41 @@
 extends RigidBody2D
 
+var paper = "Paper"
+var scissors = "Scissors"
+var rock = "Rock"
+
+var paperImage = preload("res://art/paper.png")
+var scissorsImage = preload("res://art/scissors.png")
+var rockImage = preload("res://art/rock.png")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(name)
+	if (paper in name):
+		$Sprite.texture = paperImage
+	elif (rock in name):
+		$Sprite.texture = rockImage
+	elif (scissors in name):
+		$Sprite.texture = scissorsImage
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# print(name)
 	pass
+
+func _on_Rock_body_entered(body):
+	if (paper in name):
+		if (scissors in body.name):
+			$Sprite.set_texture(scissorsImage)
+			name = scissors
+	elif (scissors in name):
+		if (rock in body.name):
+			$Sprite.set_texture(rockImage)
+			name = rock
+	elif (rock in name):
+		if (paper in body.name):
+			$Sprite.set_texture(paperImage)
+			name = paper
+	pass # Replace with function body.
